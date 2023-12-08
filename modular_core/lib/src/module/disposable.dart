98ff1350639eadf6593f3ed04eff50d5852f04a5
vatsal tanna <vatsal.t@simformsolutions.com>
2023-12-08 22:53:23 +0530

    Update pubspec

--- modular_core/lib/src/module/disposable.dart
+++ modular_core/lib/src/module/disposable.dart
@@ -3,7 +3,7 @@ part of '../../modular_core.dart';
 /// A class which implements [Disposable] can be disposed automatically
 /// once user leaves a Module
 //ignore:one_member_abstracts
-mixin abstract class Disposable {
+abstract mixin class Disposable {
   /// Disposes controllers, streams, etc.
   void dispose();
 }
